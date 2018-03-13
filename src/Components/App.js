b24db1c2ecc6805500ff670a5b1317a1cea03bfc
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-12 21:19:37 -0300

    edit app title

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from 'material-ui/Button';
 import '../Styles/App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Button variant="raised" color="primary">
+        Hello World
+      </Button>
+    </div>
   );
 }
 
