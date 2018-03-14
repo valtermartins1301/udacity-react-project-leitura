08e2e47f59223ec4420c9cb477967d3f009ff515
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-14 10:33:18 -0300

    create style structure

---
+++ src/styles/App.js
@@ -0,0 +1,7 @@
+const styles = () => ({
+  app: {
+    textAlign: 'center',
+  },
+});
+
+export default styles;
---
+++ src/styles/Root.js
@@ -0,0 +1,16 @@
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+export default styles;
