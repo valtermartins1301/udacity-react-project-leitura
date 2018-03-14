17ccf32d4df3e9e3c02c5d61b7a3982ec841d6be
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-14 20:21:39 -0300

    reducers facade

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import comment from './comment';
+import post from './post';
 
-export default combineReducers({});
+export default combineReducers({
+  comment,
+  post,
+});
