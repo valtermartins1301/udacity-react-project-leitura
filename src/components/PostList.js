95de091255a5e2bc900b73c64515bed4fed0fb2f
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-15 21:55:42 -0300

    add sort options on post list

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,20 +7,43 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/post';
 import styles from '../styles/App';
 import Post from './Post';
+import SortBy from './SortBy';
 
 class Posts extends Component {
+  state = {
+    sortBy: 'voteScore',
+    sortOptions: [{
+      key: 'voteScore',
+      value: 'Votos',
+    }, {
+      key: 'timestamp',
+      value: 'Data de criação',
+    }],
+  }
+
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(fetchPosts());
   }
 
+  onSortChange = (event, value) => {
+    this.setState({ sortBy: value });
+  }
+
   render() {
     const { items, classes } = this.props;
-    const sortedItems = items.sort((a, b) => a.voteScore < b.voteScore);
+    const { sortBy, sortOptions } = this.state;
+    const sortedItems = items.sort((a, b) => a[sortBy] < b[sortBy]);
 
     return (
       <div className={classes.app}>
         <Grid container spacing={24}>
+          <SortBy
+            label="Ordenar por:"
+            options={sortOptions}
+            defaultValue={sortBy}
+            handleChange={this.onSortChange}
+          />
           {sortedItems.map(post => (
             <Grid item xs={4} key={post.id}>
               <Post post={post} />
