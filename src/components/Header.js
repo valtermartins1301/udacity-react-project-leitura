80edacc035993603e413141ea7e2a9cf7707da53
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-15 23:42:41 -0300

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Typography from 'material-ui/Typography';
+import Toolbar from 'material-ui/Toolbar';
+import styles from '../styles/Header';
+
+const Header = ({ classes }) => (
+  <div classNames={classes.root}>
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Typography variant="title" color="inherit" className={classes.flex}>
+          The BlogPost
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+);
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
---
+++ src/styles/Header.js
@@ -0,0 +1,10 @@
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+};
+
+export default styles;
