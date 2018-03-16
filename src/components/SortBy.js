e9027fcef258e8cd35ae231bcd71740e7ce97593
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-15 21:47:59 -0300

    create SortBy component

---
+++ src/components/SortBy.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Radio, { RadioGroup } from 'material-ui/Radio';
+import { FormLabel, FormControl, FormControlLabel } from 'material-ui/Form';
+import styles from '../styles/SortBy';
+
+const SortBy = ({
+  classes, label, options, defaultValue, handleChange,
+}) => (
+  <div className={classes.root}>
+    <FormControl component="fieldset" className={classes.formControl}>
+      <FormLabel component="legend">{label}</FormLabel>
+      <RadioGroup
+        aria-label={label}
+        className={classes.group}
+        value={defaultValue}
+        onChange={handleChange}
+      >
+        {options.map(option => (
+          <FormControlLabel value={option.key} control={<Radio />} label={option.value} />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  </div>
+);
+
+SortBy.propTypes = {
+  label: PropTypes.string.isRequired,
+  defaultValue: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SortBy);
---
+++ src/styles/SortBy.js
@@ -0,0 +1,13 @@
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  formControl: {
+    margin: theme.spacing.unit * 3,
+  },
+  group: {
+    margin: `${theme.spacing.unit}px 0`,
+  },
+});
+
+export default styles;
