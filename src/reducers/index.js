ba82bbe44241338764e27ee92b4e4bf88e5bc268
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-17 01:48:17 -0300

    include Category reduce on reducers list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import comment from './comment';
 import posts from './posts';
+import categories from './categories';
 
 export default combineReducers({
   comment,
   posts,
+  categories,
 });
