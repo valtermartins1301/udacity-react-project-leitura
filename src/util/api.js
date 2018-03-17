b01f8b01f568a671f0052e6dbbce10d98cc414ce
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-17 01:48:46 -0300

    add API categories fetch

--- src/util/api.js
+++ src/util/api.js
@@ -1,20 +1,33 @@
 import postFactory from '../factories/Post';
+import categoryFactory from '../factories/Category';
 
 const SERVER_IP = process.env.REACT_APP_SERVER_IP;
 const SERVER_PORT = process.env.REACT_APP_SERVER_PORT;
 
-export const fetchAllPosts = async () => {
-  const url = `http://${SERVER_IP}:${SERVER_PORT}/posts`;
+const fetchResource = async (resource) => {
+  const url = `http://${SERVER_IP}:${SERVER_PORT}/${resource}`;
   const options = {
     headers: { Authorization: 'whatever-you-want' },
   };
 
   const response = await fetch(url, options);
-  const parsedResponse = await response.json();
 
-  return parsedResponse.map(item => postFactory.create(item));
+  return response.json();
+};
+
+export const fetchAllPosts = async () => {
+  const response = await fetchResource('posts');
+
+  return response.map(item => postFactory.create(item));
+};
+
+export const fetchAllCategories = async () => {
+  const { categories } = await fetchResource('categories');
+
+  return categories.map(item => categoryFactory.create(item));
 };
 
 export default {
   fetchAllPosts,
+  fetchAllCategories,
 };
