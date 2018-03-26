20db253373f499a6f28f50fd1b85ef6c705fdd3b
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-25 23:04:34 -0300

    create persistPost action

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,12 +1,19 @@
-import { fetchAllPosts } from '../util/api';
+import { fetchAllPosts, createPost } from '../util/api';
 
 export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const PERSIST_POST = 'PERSIST_POST';
 
 export const receivePosts = posts => ({
   type: RECEIVE_POSTS,
   posts,
   receivedAt: Date.now(),
 });
+
+export const persistPost = (...attributes) => dispatch => (
+  createPost(...attributes)
+    .then(() => fetchAllPosts())
+    .then(posts => dispatch(receivePosts(posts)))
+);
 
 export const fetchPosts = () => dispatch => (
   fetchAllPosts()
