2726304f702d4d5c7da6b8abf6de2f8c94df5bf2
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-25 23:02:18 -0300

    change API to create posts

--- src/util/api.js
+++ src/util/api.js
@@ -4,13 +4,17 @@ import categoryFactory from '../factories/Category';
 const SERVER_IP = process.env.REACT_APP_SERVER_IP;
 const SERVER_PORT = process.env.REACT_APP_SERVER_PORT;
 
-const fetchResource = async (resource) => {
+const fetchResource = async (resource, options) => {
   const url = `http://${SERVER_IP}:${SERVER_PORT}/${resource}`;
-  const options = {
-    headers: { Authorization: 'whatever-you-want' },
+  const requestOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'whatever-you-want',
+    },
+    ...options,
   };
 
-  const response = await fetch(url, options);
+  const response = await fetch(url, requestOptions);
 
   return response.json();
 };
@@ -31,6 +35,24 @@ export const fetchAllCategories = async () => {
   const { categories } = await fetchResource('categories');
 
   return categories.map(item => categoryFactory.create(item));
+};
+
+export const createPost = ({
+  title, body, author, category,
+}) => {
+  const post = {
+    id: Date.now(),
+    timestamp: Date.now(),
+    title,
+    body,
+    author,
+    category,
+  };
+
+  return fetchResource('posts', {
+    method: 'POST',
+    body: JSON.stringify(post),
+  });
 };
 
 export default {
