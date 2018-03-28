6d5a5f8c026582e769529e23e50a3b9f4f0e2f65
valtermartins1301 <valtermartins.1301@gmail.com>
2018-03-27 22:19:39 -0300

    add receive post action

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,11 +1,22 @@
-import { fetchAllPosts, createPost } from '../util/api';
+import {
+  fetchAllPosts,
+  createPost,
+  fetchPostById,
+} from '../util/api';
 
 export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 export const PERSIST_POST = 'PERSIST_POST';
+export const RECEIVE_POST = 'RECEIVE_POST';
 
 export const receivePosts = posts => ({
   type: RECEIVE_POSTS,
   posts,
+  receivedAt: Date.now(),
+});
+
+export const receivePost = post => ({
+  type: RECEIVE_POST,
+  post,
   receivedAt: Date.now(),
 });
 
@@ -18,4 +29,9 @@ export const persistPost = (...attributes) => dispatch => (
 export const fetchPosts = () => dispatch => (
   fetchAllPosts()
     .then(posts => dispatch(receivePosts(posts)))
+);
+
+export const fetchPost = id => dispatch => (
+  fetchPostById(id)
+    .then(post => dispatch(receivePost(post)))
 );
